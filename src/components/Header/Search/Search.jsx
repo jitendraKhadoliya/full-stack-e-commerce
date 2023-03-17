294bc9ed4b3c6394b74946650e4973039188ee13
jitendra <jitendrasaini352jjnu@gmail.com>
2023-03-17 15:14:44 +0530

    completed search cart fuctionality

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,12 +1,14 @@
 import { MdClose } from "react-icons/md";
 import "./Search.scss";
-import prod from "../../../assets/products/watch-prod-3.webp";
 import { useState } from "react";
 import useFetch from "../../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 const Search = ({ setShowSearch }) => {
   // * here i will create state to collect the data from input tag
   const [query, setQuery] = useState("");
+
+  const navigate = useNavigate();
 
   const onChangeFunction = (e) => {
     setQuery(e.target.value);
@@ -35,9 +37,16 @@ const Search = ({ setShowSearch }) => {
       </div>
       {/* i will do the resulted stuffed  */}
       <div className="search-result-content">
-        {data?.data?.map((item) => {
-          <div key={item.id} className="search-results">
-            <div className="search-result-item">
+        <div className="search-results">
+          {data?.data?.map((item) => (
+            <div
+              key={item.id}
+              className="search-result-item"
+              onClick={() => {
+                navigate("/product/" + item.id);
+                setShowSearch(false);
+              }}
+            >
               <div className="img-container">
                 <img
                   src={
@@ -52,8 +61,9 @@ const Search = ({ setShowSearch }) => {
                 <span className="desc">{item.attributes.desc}</span>
               </div>
             </div>
-          </div>;
-        })}
+          ))}
+        </div>
+        ;
       </div>
     </div>
   );
